da5fb53876e19f24748e354deb9fe5f58d5ca4c3
yeahitsok22 <yeahitsok22@gmail.com>
2025-05-25 13:37:05 +0300

    Update save.php

--- save.php
+++ save.php
@@ -1,13 +1,9 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = htmlspecialchars($_POST['username']);
-    $password = htmlspecialchars($_POST['password']);
-    
-    $data = "Username: $username | Password: $password\n";
-    
-    // يفتح الملف وبيكتب عليه
-    file_put_contents("users.txt", $data, FILE_APPEND);
-    
-    echo "تم التسجيل بنجاح!";
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    echo "Username: $username <br> Password: $password";
+} else {
+    echo "Invalid request.";
 }
 ?>
